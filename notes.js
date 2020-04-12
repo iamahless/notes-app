9b24d5bf70276b1b18b97a5ec4e81966278d9790
Alexander Garuba <ahless@Alexanders-MBP.local>
2020-04-12 18:38:39 +0100

    Complete Notes App

---
+++ notes.js
@@ -0,0 +1,76 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+const getNotes = () => 'Your notes...';
+
+const addNote = (title, body) => {
+  const notes = loadNotes();
+  const duplicateNote = notes.find((note) => note.title === title);
+
+  if (!duplicateNote) {
+    notes.push({
+      title: title,
+      body: body
+    });
+    saveNotes(notes);
+    console.log(chalk.green.inverse.bold('New note added!'));
+  } else {
+    console.log(chalk.red.inverse.bold('Note title taken'));
+  }
+}
+
+const removeNote = (title) => {
+  const notes = loadNotes();
+
+  const notesToKeep = notes.filter((note) => note.title !== title);
+
+  if (notes.length > notesToKeep.length) {
+    saveNotes(notesToKeep);
+    console.log(chalk.green.inverse.bold('Note removed'));
+  } else {
+    console.log(chalk.red.inverse.bold('No note found'))
+  }
+
+}
+
+const listNotes = () => {
+  const notes = loadNotes();
+  console.log(chalk.inverse('Your notes'));
+  notes.forEach((note) => {
+    console.log(note.title)
+  })
+}
+
+const readNote = (title) => {
+  const notes = loadNotes();
+  const note = notes.find((note) => note.title === title);
+  if (note) {
+    console.log(chalk.inverse(note.title))
+    console.log(note.body)
+  } else {
+    console.log(chalk.red.inverse('Note not found'))
+  }
+}
+
+const saveNotes = (notes) => {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync('notes.json', dataJSON);
+}
+
+const loadNotes = () => {
+  try {
+    const dataBuffer = fs.readFileSync('notes.json');
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    return [];
+  }
+}
+
+module.exports = {
+  getNotes: getNotes,
+  addNote: addNote,
+  removeNote: removeNote,
+  listNotes: listNotes,
+  readNote: readNote
+};
